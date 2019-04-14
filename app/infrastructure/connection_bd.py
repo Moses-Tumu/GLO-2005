923c9da23279aad9837611ba1c48750131b6a550
Moses Tumuhambaze <moses.tumuhambaze.1@ulaval.ca>
2019-04-14 14:35:25 -0400

    modification du script de creation des tables

--- app/infrastructure/connection_bd.py
+++ app/infrastructure/connection_bd.py
@@ -6,68 +6,67 @@ class UserRepository:
     PORT = '3306'
     USERNAME = 'root'
     PASSWORD = None
-    DATABASE_NAME = 'universite'
+    DATABASE_NAME = 'GLO2005'
 
 # TODO: Modifier les elements de retour pour retourner les bons éléments.
     def __init__(self):
         self.connector = connect(host=self.MYSQL_URI, port=self.PORT, user=self.USERNAME, password=self.PASSWORD, database=self.DATABASE_NAME)
 
     def getusers(self):
-        query = "SELECT * FROM users"
+        query = "SELECT * FROM User"
 
         cursor = self.connector.cursor()
         cursor.execute(query)
 
-        return [{'lastName': user[1], 'firstName': user[0]} for user in cursor]
+        return [{'LastName': user[2], 'FirstName': user[1], 'UserName':user[2]} for user in cursor]
 
     def getmovies(self):
-        query = "SELECT * FROM movies"
+        query = "SELECT * FROM Movie"
 
         cursor = self.connector.cursor()
         cursor.execute(query)
 
-        return [{'lastName': movie[1], 'firstName': movie[0]} for movie in cursor]
+        return [{'Title': movie[1], 'Synopsis': movie[2], 'Length': movie[3], 'Year': movie[4],
+                 'Country': movie[5], 'MaturityRating': movie[6], 'ImageUrl': movie[7]} for movie in cursor]
 
     def getmovies(self, type):
-        query = "SELECT * FROM movies WHERE type = %s"
+        query = ("SELECT * FROM Movie "
+                 "JOIN Genre ON Genre.GenreId = Movie.GenreId"
+                 "WHERE Genre.Name = %s")
         query_values = type
 
         cursor = self.connector.cursor()
         cursor.execute(query, query_values)
 
-        return [{'lastName': movie[1], 'firstName': movie[0]} for movie in cursor]
+        return [{'Title': movie[1], 'Synopsis': movie[2], 'Length': movie[3], 'Year': movie[4],
+                 'Country': movie[5], 'MaturityRating': movie[6], 'ImageUrl': movie[7]} for movie in cursor]
 
     def gettvshows(self):
-        query = "SELECT * FROM tvshows"
+        query = "SELECT * FROM Shows"
 
         cursor = self.connector.cursor()
         cursor.execute(query)
 
-        return [{'lastName': tvshow[1], 'firstName': tvshow[0]} for tvshow in cursor]
+        return [{'Title': tvshow[1], 'Synopsis': tvshow[2], 'FromYear': tvshow[3], 'ToYear': tvshow[4],
+                 'Country': tvshow[5], 'MaturityRating': tvshow[6], 'ImageUrl': tvshow[7]} for tvshow in cursor]
 
     def gettvshows(self, type):
-        query = "SELECT * FROM tvshows WHERE type = %s"
+        query = ("SELECT * FROM TvShow "
+                 "JOIN Genre ON Genre.GenreId = TvShow.GenreId"
+                 "WHERE Genre.Name = %s")
         query_values = type
 
         cursor = self.connector.cursor()
-        cursor.execute(query, query_values);
+        cursor.execute(query, query_values)
 
-        return [{'lastName': tvshow[1], 'firstName': tvshow[0]} for tvshow in cursor]
+        return [{'Title': tvshow[1], 'Synopsis': tvshow[2], 'FromYear': tvshow[3], 'ToYear': tvshow[4],
+                 'Country': tvshow[5], 'MaturityRating': tvshow[6], 'ImageUrl': tvshow[7]} for tvshow in cursor]
 
-    def gettvshowstype(self):
-        query = "SELECT * FROM tvshows WHERE type = %s"
+    def getgenre(self):
+        query = "SELECT * FROM Genre"
         query_values = type
 
         cursor = self.connector.cursor()
-        cursor.execute(query, query_values);
+        cursor.execute(query, query_values)
 
-        return [{'lastName': tvshow[1], 'firstName': tvshow[0]} for tvshow in cursor]
-
-    def getmoviestype(self):
-        query = "SELECT * FROM tvshows WHERE type = %s"
-        query_values = type
-
-        cursor = self.connector.cursor()
-        cursor.execute(query, query_values);
-
-        return [{'lastName': tvshow[1], 'firstName': tvshow[0]} for tvshow in cursor]
+        return [{'Name': genre[0]} for genre in cursor]
